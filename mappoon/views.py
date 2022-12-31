aa3f1d1ad3343e5b55ccb0224f86d5809d91e6d6
hooman <hoomanlangrodi@gmail.com>
2022-12-31 12:02:32 +0100

    created urls and modified views

--- mappoon/views.py
+++ mappoon/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views import View
+
+class home(View):
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
+    
