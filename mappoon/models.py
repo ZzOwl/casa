9ba0158f30f68decb2fb141654a930a541fd9358
hooman <hoomanlangrodi@gmail.com>
2023-01-05 22:30:33 +0100

    data base

--- mappoon/models.py
+++ mappoon/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 class menu(models.Model):
-    menu_cat_name = models.CharField(max_length=200)
+    menu_category_name = models.CharField(max_length=200)
 
-class menu_cat(models.Model):
+class menu_category(models.Model):
     menu_item = models.CharField(max_length=40)
     price = models.IntegerField(null=False)
-    cat_id = models.ForeignKey(menu, on_delete=models.PROTECT, default=None)
+    category_id = models.ForeignKey(menu, on_delete=models.PROTECT, default=None, related_name='category_name')
     
 class customers(models.Model):    
     id = models.PositiveIntegerField(primary_key = True)
