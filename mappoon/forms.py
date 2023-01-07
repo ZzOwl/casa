9842e1ced97dac10a943176ee788e314e1c4933a
hooman <hoomanlangrodi@gmail.com>
2023-01-07 13:37:05 +0100

    created forms

---
+++ mappoon/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms.widgets import NumberInput    #for reservation_date
+
+class DemoForm(forms.Form):
+    name = forms.CharField(widget=forms.Textarea(attrs={'rows':5}))  # attr will determine the size of the field
+    email = forms.EmailField(label='enter the Email')            #label text will replace the email
+    reservation_date = forms.DateField(widget=NumberInput(attrs={'type' : 'date'}))
+    
+    DISH_LIST = [
+        ('italian', 'Italian')
+        ('greek', 'Greek')                  #for favorite_dish
+        ('chinese', 'Chinese')
+    ]
+    
+    favorite_dish = forms.ChoiceField(widget=forms.RadioSelect, choices= DISH_LIST)      #by using radio select all choices will be shown at once
