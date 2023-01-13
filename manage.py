1efd477eaabc35e61fd2ccdf006aba375a01c7b5
Grazie <moein.zeidanlou@gmail.com>
2023-01-13 14:59:13 +0100

    confirm id from users

--- manage.py
+++ manage.py
@@ -8,13 +8,20 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inproj.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+        sure_input = int(input("Type your id here :"))
+        print("your id {0} confirmed.".format(sure_input))
+    except ImportError as exc :
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    except ValueError as e :
+        raise ImportError(
+            "Just integer type is acceptable. Are you sure you enter your number !"
+            "juster enter your number id"
+        ) from e
+    execute_from_command_line(sys.argv)  
 
 
 if __name__ == '__main__':
